fdb46c5e304e7f802ebe81f6bf024d5c89e64c2f
AnfalHussain <anfalae95@gmail.com>
2019-08-20 14:44:55 +0300

    Anfal Hussain & Abdullah Alhasan

--- restaurants/views.py
+++ restaurants/views.py
@@ -6,14 +6,24 @@ def welcome(request):
 def restaurant_list(request):
 
     context = {
-
+    	'my_list':[{
+    	'restaurant_name':"ChocolateBar",
+    	'food_type':"Desserts",},
+    	{'restaurant_name':"KFC",
+    	'food_type':"Fried Chicken",},
+    	{
+    	'restaurant_name':"Junkyard",
+    	'food_type':"Junk Food",},]
     }
     return render(request, 'list.html', context)
 
 
 def restaurant_detail(request):
 
-    context = {
-
+    context = { 
+    	'my_object':{
+    	"restaurant_name" :"KFC",
+    	'food_type':"Fried Chicken",
+    	}
     }
     return render(request, 'detail.html', context)
